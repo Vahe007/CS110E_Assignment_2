283ea3c6fce66574f088590fd97e669c13f14e01
Vahe007 <92799670+Vahe007@users.noreply.github.com>
2021-10-19 23:19:56 +0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,76 @@
+//Problem N1
+
+//const x = 5; // height of the triangle or one of the recange
+// const y = 4; // base of the triange or another side of rectange
+
+
+// const areaTrangle = (x * y) / 2;
+// const areaRectange = x * y;
+
+// if (areaRectange > areaTrangle) {
+//     console.log("The area of the Recange is greater");
+// }
+// else if (areaRectange < areaTrangle) {
+//     console.log("The area of the Triangle is greater");
+// }
+// else {
+//     console.log("Input other numbers which are greater than 0");
+// }
+
+
+
+
+
+
+
+//Problem N2
+// we have a*x^2 + b*x + c = 0 equation.
+
+// const a = 1;
+// const b = -4;
+// const c = -3;
+
+// if (a === 0) {
+//     let x = -c / b;
+//     console.log(x);
+//     return;
+// }
+// let d = (b ** 2) - (4 * a * c);
+// if (d < 0) {
+//     console.log("The equation has no solutions");
+// }
+// else if (d > 0) {
+//     let x1 = (-b + Math.sqrt(d)) / (2 * a);
+//     let x2 = (-b - Math.sqrt(d)) / (2 * a);
+//     console.log(`The first number is ${x1}`);
+//     console.log(`The second number is ${x2}`);
+// }
+// else if (d === 0) {
+//     let x0 = (-b) / (2 * a);
+//     console.log((`The only solution is ${x0}`));
+// }
+
+
+
+
+
+
+//Problem N3
+
+// const x1 = 0;
+// const y1 = 0;
+// const x2 = 0;
+// const y2 = -5;
+
+// const length = Math.sqrt(((x2 - x1) ** 2) + ((y2 - y1) ** 2));
+
+// console.log("The length of the line is " + length);
+
+
+
+
+//Problem N4
+
+
+console.log("ok");
+
